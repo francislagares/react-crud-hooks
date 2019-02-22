eb161362eceaf3fabf02cc12f7e23f05660d79cf
Francis Lagares <francis.lagares@gmail.com>
2019-02-22 18:07:31 +0100

    Added EditUserForm and Fragment components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useState  } from 'react';
+import React, { Fragment, useState  } from 'react';
 import AddUserForm from './forms/AddUserForm';
+import EditUserForm from './forms/EditUserForm';
 import UserTable from './tables/UserTable';
 
 const App = () => {
@@ -9,7 +10,11 @@ const App = () => {
     { id: 3, name: 'Ben', username: 'benishere' },
   ]
 
+  const initialFormState = { id: null, name: '', username: '' }
+
   const [ users, setUsers ] = useState(usersData);
+  const [ currentUser, setCurrentUser ] = useState(initialFormState);
+  const [ editing, setEditing ] = useState(false);
 
   const addUser = user => {
     user.id = users.length + 1;
@@ -19,18 +24,42 @@ const App = () => {
   const deleteUser = id => {
     setUsers(users.filter(user => user.id !== id));
   }
+
+  const editUser = user => {
+    setEditing(true);
+    setCurrentUser({ id: user.id, user: user.name, username: user.username })
+  }
+
+  const updateUser = (id, updatedUser) => {
+    setEditing(false);
+    setUsers(users.map( user => ( user.id === id ? updateUser : user )));
+  }
   
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
       <div className="row">
         <div className="col-md-6">
-          <h2>Add user</h2>
-          <AddUserForm addUser={addUser} />
+        { editing ? (
+          <Fragment>
+            <h2>Edit User</h2>
+            <EditUserForm 
+              editing={editing}
+              setEditing={setEditing}
+              currentUser={currentUser}
+              updateUser={updateUser} />
+          </Fragment>
+          ) : (
+          <Fragment>
+            <h2>Add user</h2>
+            <AddUserForm addUser={addUser} />
+          </Fragment>
+          )
+        }
         </div>
         <div className="col-md-6">
           <h2>View users</h2>
-          <UserTable users={users} deleteUser={deleteUser} />
+          <UserTable users={users} deleteUser={deleteUser} editUser={editUser} />
         </div>
       </div>
     </div>
